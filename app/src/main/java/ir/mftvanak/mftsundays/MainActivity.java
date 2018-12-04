a17fc67cd224300bc156da770e5b670d71732668
Pouya <pouyaheydary@gmail.com>
2018-12-04 18:09:03 +0330

    session 10

--- app/src/main/java/ir/mftvanak/mftsundays/MainActivity.java
+++ app/src/main/java/ir/mftvanak/mftsundays/MainActivity.java
@@ -1,58 +1,53 @@
 package ir.mftvanak.mftsundays;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.first_activity);
+
+//        final TextView textView = findViewById(R.id.txt);
+//
+//        EditText editText = findViewById(R.id.edt);
+//        String s = editText.getText().toString();
+//
+//
+//
+//        Button button = findViewById(R.id.btnChangeText);
+//        button.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//                Toast.makeText(MainActivity.this,"Pouya",Toast.LENGTH_LONG).show();
+//
+//            }
+//        });
+//
 
 
-        Log.d("pouya", "on Create Called");
+        Button button = findViewById(R.id.btnNewActivity);
 
-    }
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-    @Override
-    protected void onStart() {
-        super.onStart();
+                Intent intent = new Intent(MainActivity.this,SecondActivity.class);
+                intent.putExtra("contentOfText","Salam");
 
-    }
+                startActivity(intent);
 
+            }
+        });
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-    }
-
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-    }
-
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-    }
-
-
-    @Override
-    public void onBackPressed() {
-        super.onBackPressed();
     }
 }
---
+++ app/src/main/java/ir/mftvanak/mftsundays/SecondActivity.java
@@ -0,0 +1,25 @@
+package ir.mftvanak.mftsundays;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class SecondActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        Intent intent = getIntent();
+
+        String s = intent.getStringExtra("contentOfText");
+
+
+        TextView textView = findViewById(R.id.txtShowText);
+
+        textView.setText(s);
+
+
+    }
+}
