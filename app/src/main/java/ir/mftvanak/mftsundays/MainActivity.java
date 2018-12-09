d452daf5c4c5bf01d32cb1cff336effd6f40df8c
Pouya <pouyaheydary@gmail.com>
2018-12-09 18:16:57 +0330

    end of session 11

--- app/src/main/java/ir/mftvanak/mftsundays/MainActivity.java
+++ app/src/main/java/ir/mftvanak/mftsundays/MainActivity.java
@@ -1,15 +1,12 @@
 package ir.mftvanak.mftsundays;
 
-import android.app.Activity;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,49 +15,37 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.first_activity);
 
-//        final TextView textView = findViewById(R.id.txt);
-//
-//        EditText editText = findViewById(R.id.edt);
-//        String s = editText.getText().toString();
-//
-//
-//
-//        Button button = findViewById(R.id.btnChangeText);
-//        button.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View v) {
-//
-//                Toast.makeText(MainActivity.this,"Pouya",Toast.LENGTH_LONG).show();
-//
-//            }
-//        });
-//
+        Button btn = findViewById(R.id.txtToast);
+        Button btnNewActivity = findViewById(R.id.btnNewActivity);
 
 
-        Button button = findViewById(R.id.btnNewActivity);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog dialog = new AlertDialog.Builder(MainActivity.this).create();
+                dialog.setTitle("Attention !");
+                dialog.setMessage("This dialog created by MFT students");
+                dialog.setButton(AlertDialog.BUTTON_POSITIVE, "I understand", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        dialog.dismiss();
+                    }
+                });
 
-        button.setOnClickListener(new View.OnClickListener() {
+                dialog.show();
+
+            }
+        });
+
+
+        btnNewActivity.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, SubActivity.class);
 
-                startActivityForResult(intent, 1400);
+                startActivity(intent);
             }
         });
-
-    }
-
-
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
-
-        String s;
-        super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == 1400)
-            if (resultCode == Activity.RESULT_OK) {
-                s = data.getStringExtra("result");
-                Toast.makeText(MainActivity.this, s, Toast.LENGTH_LONG).show();
-            }
 
 
     }
